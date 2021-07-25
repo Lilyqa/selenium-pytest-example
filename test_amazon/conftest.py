07130724bfe1838b5cd39fe4b8470e0f84676d40
Lilyqa <lily.mykhailovaqate@gmail.com>
2021-07-25 19:05:54 -0400

    added con

---
+++ test_amazon/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+@pytest.fixture()
+def browser():
+    browser = webdriver.Chrome(ChromeDriverManager().install())
+    browser.implicitly_wait(5)
+    yield browser
